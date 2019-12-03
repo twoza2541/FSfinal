0537386774905c652aa005400e7f104d6f2061fc
twoza2541 <56666234+twoza2541@users.noreply.github.com>
2019-12-03 18:16:44 +0700

    4

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var express = require('express');
 // ===============================
 
 var app = express();   
-var cors = require('cors');  
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-var options = { useUnifiedTopology: true, useNewUrlParser: true };     
+var cors = require('cors');   
 
 // #2 Add body-parser package to the app
 var bodyParser = require('body-parser');
@@ -23,8 +19,20 @@ app.use(bodyParser.json());
 
 // #3 Serve static content in folder frontend
 app.use(express.static('www'));
-// ===============================
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/coc',
+    { useUnifiedTopology: true, useNewUrlParser: true });
+var Product = require('./models/product');
 
+app.get('/', function (req, res) {
+    res.send('Express is running');
+});
+
+app.get('/api', function (req, res) {
+    var version = { version: "1.0b" };
+    res.json(version);
+});
+// ===============================
 
 var port = process.env.PORT || 8080; 
 
@@ -37,7 +45,11 @@ router.get('/products', products.getAllProducts);
 router.get('/products/:pid', products.getProductById);
 
 // #4 Complete the routing for POST, PUT, DELETE
-
+router.get('/products', products.getAllProducts);
+router.get('/products/:pid', products.getProductsById);
+router.post('/products', products.addProducts);
+router.put('/products/:pid', products.updateProductsById);
+router.delete('/products/:pid', products.deleteProductsById);
 // ===============================
 
 
@@ -46,6 +58,5 @@ router.get('/products/:pid', products.getProductById);
 app.use('/api', cors(), router);
 
 // #10 Start the server
-
 // ===============================
 console.log('Magic happens on http://localhost:' + port);
